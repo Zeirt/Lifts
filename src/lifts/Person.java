06b4a186a5cae74b3f4ccbff47993f8f0e4394f7
Beatriz Cortés <beatriz.cortes@edu.uah.es>
2016-04-17 20:50:09 +0200

    Forgot to update person
    what the heck github?

--- src/lifts/Person.java
+++ src/lifts/Person.java
@@ -1,5 +1,8 @@
 package lifts;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Spawns in a floor and calls the lift to go to destination.
  * @author Beatriz Cortés Sánchez
@@ -10,6 +13,7 @@ public class Person extends Thread{
     private int position;
     private int destination;
     private Controller controller;
+    private Lift liftIn;//"-" if nothing. Then "l1" or "l2"
     
     /**
      * Constructor of Person. 
@@ -23,6 +27,7 @@ public class Person extends Thread{
         this.position = position;
         this.destination = destination;
         this.controller = controller;
+        liftIn = null;
         this.start();
     }
     
@@ -47,11 +52,20 @@ public class Person extends Thread{
             System.out.println("I want lift to pick me up in floor " + position);
             controller.call(position);
             System.out.println("I'm going to enter the elevator.");
-            controller.enterElevator(this);
+            liftIn = controller.enterElevator(this);
             System.out.println("I want to go to floor " + destination);
-            controller.call(destination);
+            controller.requestStop(destination);
+            while(liftIn.getLiftLocation()!=destination && liftIn.isWorking()){
+                try {
+                    //may get woken up by accident. Make sure they don't leave
+                    liftIn.wait();
+                } catch (InterruptedException ex) {
+                    System.out.println("InterruptedException caught in Person run()");
+                }
+            }
             System.out.println("I'm leaving the elevator.");
             controller.exitElevator(this);
+            liftIn = null;
             System.out.println("I'm in floor " + position);
         }
         System.out.println("I'm going to end.");
